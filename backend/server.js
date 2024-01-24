3ee8d56c3db1ede2c0795622d879d2989ec7d68b
sheikhnaved <75406847+sheikhnaved135@users.noreply.github.com>
2024-01-25 03:24:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,19 @@ import authRoutes from "./routes/auth.routes.js";
 import cors from "cors";
 import categoryRoutes from "./routes/category.routes.js";
 import productRoutes from "./routes/product.routes.js";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 const app = express();
 
 //configure env
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./client/dist")));
+// app.use(express.static(path.join(__dirname, "./client/dist")));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
@@ -29,11 +29,11 @@ connectDb();
 
 //rest api
 const PORT = process.env.PORT || 8080;
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
-});
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+// });
 app.get("/", (req, res) => {
-  res.send({ message: req.params });
+  res.send({ message: "hello commerce" });
 });
 
 app.listen(PORT, () => {
