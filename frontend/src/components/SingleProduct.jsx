4880633f8ab9df33126f1079c061665d8e4f260b
sheikhnaved135 <sheikhnaved022@gmail.com>
2024-01-25 03:54:52 +0530

    deleted

--- frontend/src/components/SingleProduct.jsx
+++
@@ -1,8 +0,0 @@
-import { Layout } from "antd";
-import React from "react";
-
-const SingleProduct = () => {
-  return <Layout title={"Update/Delete product"}></Layout>;
-};
-
-export default SingleProduct;
